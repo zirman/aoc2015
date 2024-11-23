3eced296599b8153fd701a4f749b25143391c0f5
Robert Chrzanowski <robert.chrzanowski@gmail.com>
2024-11-22 23:25:15 -0500

    Day 2

--- src/Day02.kt
+++ src/Day02.kt
@@ -1,21 +1,30 @@
 fun main() {
     fun part1(input: List<String>): Int {
-        return input.size
+        return input.sumOf {
+            val (l, w, h) = it.split('x')
+            val li = l.toInt()
+            val wi = w.toInt()
+            val hi = h.toInt()
+            val x = li * wi
+            val y = li * hi
+            val z = wi * hi
+            ((x + y + z) * 2) + x.coerceAtMost(y).coerceAtMost(z)
+        }
     }
-
     fun part2(input: List<String>): Int {
-        return input.size
+        return input.sumOf {
+            val (l, w, h) = it.split('x')
+            val li = l.toInt()
+            val wi = w.toInt()
+            val hi = h.toInt()
+            (((li + wi + hi) * 2) - (li.coerceAtLeast(wi).coerceAtLeast(hi) * 2)) + (li * wi * hi)
+        }
     }
-
-    // Test if implementation meets criteria from the description, like:
-    check(part1(listOf("test_input")) == 1)
-
-    // Or read a large test input from the `src/Day02_test.txt` file:
-    val testInput = readInput("Day02_test")
-    check(part1(testInput) == 1)
-
-    // Read the input from the `src/Day02.txt` file.
+    check(part1(listOf("2x3x4")) == 58)
+    check(part1(listOf("1x1x10")) == 43)
     val input = readInput("Day02")
     part1(input).println()
+    check(part2(listOf("2x3x4")) == 34)
+    check(part2(listOf("1x1x10")) == 14)
     part2(input).println()
 }
