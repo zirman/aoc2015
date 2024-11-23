3013f2921cc76489c939b9afa767576f2b67a748
Robert Chrzanowski <robert.chrzanowski@gmail.com>
2024-11-22 23:10:21 -0500

    Renamed days 2-9

--- src/Day01.kt
+++ src/Day01.kt
@@ -2,25 +2,22 @@ fun main() {
     fun part1(input: List<String>): Int {
         return input[0].count { it == '(' } - input[0].count { it == ')' }
     }
-
     fun part2(input: List<String>): Int {
         val steps = input[0]
         var floor = 0
-        for (i in steps.indices) {
-            when (steps[i]) {
+        steps.forEachIndexed { index, c ->
+            when (c) {
                 '(' -> floor++
                 ')' -> {
                     floor--
                     if (floor == -1) {
-                        return i + 1
+                        return index + 1
                     }
                 }
             }
         }
         throw IllegalStateException("Never entered the basement")
     }
-
-    // Test if implementation meets criteria from the description, like:
     check(part1(listOf("(())")) == 0)
     check(part1(listOf("()()")) == 0)
     check(part1(listOf("(((")) == 3)
@@ -30,10 +27,8 @@ fun main() {
     check(part1(listOf("))(")) == -1)
     check(part1(listOf(")))")) == -3)
     check(part1(listOf(")())())")) == -3)
-
     val input = readInput("Day01")
     part1(input).println()
-
     check(part2(listOf(")")) == 1)
     check(part2(listOf("()())")) == 5)
     part2(input).println()
