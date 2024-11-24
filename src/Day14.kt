60f0093ad42771f33fe7abfff6d6bd34478a58b7
Robert Chrzanowski <robert.chrzanowski@gmail.com>
2024-11-24 14:03:48 -0500

    Day 14

--- src/Day14.kt
+++ src/Day14.kt
@@ -1,15 +1,53 @@
+private data class Reindeer(val name: String, val speed: Int, val moveSeconds: Int, val restSeconds: Int)
+
 fun main() {
-    fun part1(input: List<String>): Int {
-        return input.size
+    val reindeerRegex =
+        """([a-zA-Z]+) can fly (\d+) km/s for (\d+) seconds, but then must rest for (\d+) seconds\.""".toRegex()
+
+    fun part1(input: List<String>, time: Int): Int {
+        val reindeer = input.map { line ->
+            val (name, speed, moveSeconds, restSeconds) = reindeerRegex.matchEntire(line)!!.destructured
+            Reindeer(name, speed.toInt(), moveSeconds.toInt(), restSeconds.toInt())
+        }
+        return reindeer.map { r ->
+            val cycleDistance = (time / (r.moveSeconds + r.restSeconds)) * (r.speed * r.moveSeconds)
+            val remainder = time % (r.moveSeconds + r.restSeconds)
+            cycleDistance + (remainder.coerceAtMost(r.moveSeconds) * r.speed)
+        }.max()
     }
-    fun part2(input: List<String>): Int {
-        return input.size
+
+    fun part2(input: List<String>, time: Int): Int {
+        val reindeer = input.map { line ->
+            val (name, speed, moveSeconds, restSeconds) = reindeerRegex.matchEntire(line)!!.destructured
+            Reindeer(name, speed.toInt(), moveSeconds.toInt(), restSeconds.toInt())
+        }
+        val distances = IntArray(reindeer.size)
+        val scores = IntArray(reindeer.size)
+        var i = 0
+        while (i < time) {
+            reindeer.forEachIndexed { index, r ->
+                if (i % (r.moveSeconds + r.restSeconds) < r.moveSeconds) {
+                    distances[index] += r.speed
+                }
+            }
+            val m = distances.max()
+            distances.forEachIndexed { index, d ->
+                if (d == m) {
+                    scores[index]++
+                }
+            }
+            i++
+        }
+        return scores.max()
     }
-    check(part1(listOf("test_input")) == 1)
-    val testInput = readInput("Day14_test")
-    check(part1(testInput) == 1)
+
+    val testInput = """
+        Comet can fly 14 km/s for 10 seconds, but then must rest for 127 seconds.
+        Dancer can fly 16 km/s for 11 seconds, but then must rest for 162 seconds.
+    """.trimIndent().split('\n')
+    check(part1(testInput, 1_000) == 1120)
     val input = readInput("Day14")
-    part1(input).println()
-//    check(part2(testInput) == 1)
-//    part2(input).println()
+    part1(input, 2503).println()
+    check(part2(testInput, 1_000) == 689)
+    part2(input, 2503).println()
 }
