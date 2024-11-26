b9cc15c6b76ffd34d09e4e31ad18589dad09c905
Robert Chrzanowski <robert.chrzanowski@gmail.com>
2024-11-25 22:45:25 -0500

    Day 17

--- src/Day17.kt
+++ src/Day17.kt
@@ -1,15 +1,46 @@
 fun main() {
-    fun part1(input: List<String>): Int {
-        return input.size
+    fun MutableList<IntArray>.fillContainers(containers: List<Int>, liters: Int, filledContainers: IntArray = IntArray(0)) {
+        when {
+            liters == 0 -> {
+                add(filledContainers)
+            }
+
+            containers.isEmpty() -> {
+            }
+
+            else -> {
+                val container = containers.first()
+                val remainingContainers = containers.drop(1)
+                fillContainers(remainingContainers, liters, filledContainers)
+                if (container <= liters) {
+                    fillContainers(remainingContainers, liters - container, filledContainers + container)
+                }
+            }
+        }
     }
-    fun part2(input: List<String>): Int {
-        return input.size
+
+    fun part1(input: List<String>, liters: Int): Int {
+        val containers = input.map { it.toInt() }.sortedDescending()
+        return buildList { fillContainers(containers, liters) }.size
     }
-    check(part1(listOf("test_input")) == 1)
-    val testInput = readInput("Day17_test")
-    check(part1(testInput) == 1)
+
+    fun part2(input: List<String>, liters: Int): Int {
+        val containers = input.map { it.toInt() }.sortedDescending()
+        val filledContainers = buildList { fillContainers(containers, liters) }
+        val minContainersSize = filledContainers.minOf { it.size }
+        return filledContainers.count { it.size == minContainersSize }
+    }
+
+    val testInput = """
+        20
+        15
+        10
+        5
+        5
+    """.trimIndent().split('\n')
+    check(part1(testInput, 25) == 4)
     val input = readInput("Day17")
-    part1(input).println()
-//    check(part2(listOf("test_input")) == 1)
-//    part2(input).println()
+    part1(input, 150).println()
+    check(part2(testInput, 25) == 3)
+    part2(input, 150).println()
 }
