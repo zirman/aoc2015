4a31a48c2ff79afaf20bf7a667a29deb3af1a029
Robert Chrzanowski <robert.chrzanowski@gmail.com>
2024-12-03 20:59:08 -0500

    Day 25

--- src/Day25.kt
+++ src/Day25.kt
@@ -1,15 +1,25 @@
 fun main() {
-    fun part1(input: List<String>): Int {
-        return input.size
+    val firstCode = 20151125L
+    fun Long.next(): Long = (this * 252533) % 33554393
+
+    fun part1(row: Int, col: Int): Long {
+        var r = 0
+        var c = 0
+        var i = firstCode
+        while (r != row - 1 || c != col - 1) {
+            i = i.next()
+            if (r == 0) {
+                r = c + 1
+                c = 0
+            } else {
+                r--
+                c++
+            }
+        }
+        return i
     }
-    fun part2(input: List<String>): Int {
-        return input.size
-    }
-    check(part1(listOf("test_input")) == 1)
-    val testInput = readInput("Day25_test")
-    check(part1(testInput) == 1)
-    val input = readInput("Day25")
-    part1(input).println()
-//    check(part2(listOf("test_input")) == 1)
-//    part2(input).println()
+
+    check(firstCode.next() == 31916031L)
+    check(part1(row = 2, col = 6) == 4041754L)
+    part1(row = 3010, col = 3019).println()
 }
